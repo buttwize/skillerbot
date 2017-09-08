736bb2c66beac4be83851748100733a9b091ea75
bitwise <31191954+buttwize@users.noreply.github.com>
2017-09-08 13:54:54 -0700

    let spaces in rs name for stats

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -4,9 +4,9 @@ export default {
 	params: '<username>',
 	aliases: ['stats', 'hiscore'],
 	run: function({mp, command, args, message}) {
-		const tmp = args[0];
+		const tmp = args.join(' ');
 		const name = tmp[0] === '@' ? tmp.slice(1) : tmp;
-		mp.chat(`http://services.runescape.com/m=hiscore_oldschool/hiscorepersonal.ws?user1=${name}`)
+		mp.chat(`http://services.runescape.com/m=hiscore_oldschool/hiscorepersonal.ws?user1=${encodeURI(name)}`)
 			.then(() => message.delete());
 	}
 }
