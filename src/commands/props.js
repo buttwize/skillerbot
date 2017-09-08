06219a7b4f1734b506dae960d66a01fe8f8000d0
bitwise <31191954+buttwize@users.noreply.github.com>
2017-09-08 15:26:05 -0700

    add multiple good song compliments

--- src/commands/props.js
+++ src/commands/props.js
@@ -1,3 +1,11 @@
+const compliments = [
+	'Awesome Song',
+	'Great Play',
+	'Big Tune',
+	':choooone:',
+	'Your song touched me in a special place'
+];
+
 export default {
 	name: 'Props',
 	description: 'Gives the current dj props for playing a great song.',
@@ -9,7 +17,7 @@ export default {
 			message.delete();
 			return;
 		}
-		dj.chat(`- ${message.user} gave you props for playing an awesome song!`);
+		dj.chat(`- ${message.user} gave you props!  ${compliments[Math.floor(Math.random()*compliments.length)]}!`);
 		message.delete();
 	}
 }
