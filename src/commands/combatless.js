2023e71abe4c52cd8eec8cadce400668188e752f
bitwise <31191954+buttwize@users.noreply.github.com>
2017-09-21 14:27:38 -0700

    join combatless args for names with spaces

--- src/commands/combatless.js
+++ src/commands/combatless.js
@@ -4,9 +4,9 @@ export default {
 	name: 'Combatless',
 	description: 'Gives a link to the combatless hiscores, or a profile if a name is given.',
 	params: '[name]',
-	aliases: ['combatless', 'cbl'],
+	aliases: ['cbl'],
 	run: function ({mp, command, args, message}) {
-		const res = args.length > 0 ? profile_prefix + args[0] : 'https://combatless.com/hiscores';
+		const res = args.length > 0 ? profile_prefix + args.join('_') : 'https://combatless.com/hiscores';
 		mp.chat(res)
 			.then(() => message.delete());
 	}
