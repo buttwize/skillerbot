fdc61447535e9fba0fd2b88142b50449a45c4fee
bitwise <31191954+buttwize@users.noreply.github.com>
2017-09-21 02:40:59 -0700

    update combatless command description

--- src/commands/combatless.js
+++ src/commands/combatless.js
@@ -2,8 +2,8 @@ const profile_prefix = 'https://cbl.moe/';
 
 export default {
 	name: 'Combatless',
-	description: 'RIP until combatless is back up.',
-	params: 'None',
+	description: 'Gives a link to the combatless hiscores, or a profile if a name is given.',
+	params: '[name]',
 	aliases: ['combatless', 'cbl'],
 	run: function ({mp, command, args, message}) {
 		const res = args.length > 0 ? profile_prefix + args[0] : 'https://combatless.com/hiscores';
